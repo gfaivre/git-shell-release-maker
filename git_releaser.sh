2786cf8f281ee15769888cdfe2351d5b2dc0cadc
Guewen FAIVRE <guewen.faivre@elao.com>
2017-12-18 17:17:34 +0100

    Rework of the script

--- git_releaser.sh
+++ git_releaser.sh
@@ -1,209 +1,167 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
-#title              : git_releaser.sh
-#description        : Clone/Mirror and/or update a git repository before making a release
-#date               : 20171211
-#usage              : git_releaser.sh app_name repository_url
-#notes              : Git 1.9 or greater mandatory
-#===================================================================
+# GIT releaser version 1.0.x
+# Author: Guewen FAIVRE (guewen.faivre@elao.com)
+#
+# Utility to self-creating a project release
+# The resulting directory hold the repository content at the given branch/revision
+#
+# Version history :
+# - 1.0 : Initial public release
+#
+# This software is released under the terms of the GNU GPL version 2 and above
+# Please read the license at http://www.gnu.org/copyleft/gpl.html
+#
+#=====================================================================================
 
-APP_NAME=$1
-REPOSITORY_URL=$2
+# Handle errors properly
+set -o errexit
+# Get pipelines exit codes (The exit status of the last command that threw a non-zero exit code is returned.)
+set -o pipefail
+# Force declaration of ALL variables
+set -o nounset
+# Debug (commented by default)
+#set -o xtrace
 
-ROOT_DIR="$(dirname $(pwd))"
-APPS_DIRNAME="${ROOT_DIR}/apps"
-DEPLOY_BRANCH_NAME="quantum"
-REPOSITORY_PATH=${APPS_DIRNAME}/${APP_NAME}/cached-copy
-RELEASE_PATH=${APPS_DIRNAME}/${APP_NAME}/release
+######################
+# Variables          #
+######################
 
-# Git
-SHALLOW_CLONE=0
-STRATEGY=${STRATEGY:-clone}
+# Magic variables
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__parent="$(cd "$(dirname "${__dir}")" && pwd)"
 
-# Release configuration
-BRANCH=${BRANCH:-master}
-REMOTE=${REMOTE:-origin}
-REVISION=${REVISION:-HEAD}
+# Variables
+VERSION="1.0"
 
-if [ $# -eq 0 ]; then
-  echo "No arguments provided"
-  echo "Usage:"
-  echo "git_checkout.sh APP_NAME REPOSITORY_URL"
-  exit 1
-fi
+APP_NAME=${APP_NAME:-deploy}
+readonly APPS_DIRNAME="${__dir}/apps"
+readonly REPOSITORY_CACHE_DIR=${APPS_DIRNAME}/.cached-copy/${APP_NAME}
+readonly RELEASE_PATH_DIR=${APPS_DIRNAME}/${APP_NAME}
 
-if [ -z ${REPOSITORY_URL} ];
-then
-  echo "You should provide a repository URL" >&2
-  exit 1
-fi
+# GIT variables
+BRANCH=""
+REVISION="HEAD"
 
-function display(){
+#####################
+# Utilities.        #
+#####################
+
+function display()
+{
   local message="$1"
-  printf "[\033[36mQuantum\033[0m] \033[32m${message}\033[0m\n"
+  printf "[\033[36mGIT Releaser\033[0m] \033[32m${message}\033[0m\n"
+}
+
+function display_error(){
+  local error_message="${1}"
+  printf "[\033[31mError\033[0m] \033[32m${error_message}\033[0m\n"
+}
+
+# Dump script usage syntax and options
+function releaser_Usage(){
+  echo "Usage: $0 [params] repository"
+  echo "Params can be one or more of the following :"
+  echo "    --version | -v     : Print out version number and exit"
+  echo "    --branch  | -b     : Check out corresponding branch"
 }
 
 function setup(){
   # Creating workdir env
   mkdir -p ${APPS_DIRNAME}/${APP_NAME}
-  mkdir -p ${REPOSITORY_PATH}
-  mkdir -p ${RELEASE_PATH}
+  mkdir -p ${REPOSITORY_CACHE_DIR}
+  mkdir -p ${RELEASE_PATH_DIR}
 }
 
-function is_file_exists(){
-  local fexists="$1"
-  [[ -f "${fexists}" ]] && return 0 || return 1
-}
+function is_valid_hash(){
+  local commit_hash="${1}"
 
-function cleanup(){
-  local target="$1"
-  local verbose=$2
-  local args=""
-
-  args="-rf"
-
-  # Wondering why "" ? See https://stackoverflow.com/a/2953673
-  if [ "$verbose" = true ]
+  if [[ "${commit_hash}" =~ ^[0-9a-f]{40}$ ]]
   then
-    args="${args}v"
-  fi
-
-  $(find ${target} ! -path ${target} -print0 | xargs -I {} -0 rm ${args} "{}")
-}
-
-# Get revision by listing references in the remote repository
-function fetch_revision(){
-  revision=$(git ls-remote --quiet ${REPOSITORY_URL} ${BRANCH})
-  echo ${revision}|cut -d " " -f 1
-}
-
-# Checkout a repository branch to a given revision
-function checkout(){
-  local remote="$1"
-  local branch="$2"
-  local revision="$3"
-  local destination="$4"
-  local args=""
-
-  # Add an option for the branch name so :git_shallow_clone works with branches
-  if [ "${revision}" == "${branch}" ]
-  then
-    args="${args} -b ${branch}"
-  fi
-
-  if [ "${remote}" != "origin" ]
-  then
-    args="${args} -o ${remote}"
-  fi
-
-  if [ ${SHALLOW_CLONE} -gt 0 ]
-  then
-    args="${args} --depth  ${SHALLOW_CLONE}"
-  fi
-
-  # Cloning repository (checkout to quantum branch by default to avoid mistakes)
-  cd ${destination} && \
-  $(git clone ${args} --no-single-branch ${REPOSITORY_URL} ${destination}) && \
-    git checkout -b ${DEPLOY_BRANCH_NAME} ${revision}
-}
-
-function sync_repo(){
-  local revision="$1"
-  local destination="$2"
-
-  cd ${destination}
-  git fetch origin && git fetch --tags  origin && git reset  --hard ${revision}
-  git clean -d -x -f
-}
-
-function mark(){
-  local revision="$1"
-
-  (echo ${revision} > ${RELEASE_PATH}/REVISION)
-}
-
-function clone_repo(){
-  if [ ${SHALLOW_CLONE} -gt 0 ]
-  then
-    git clone --mirror --depth ${SHALLOW_CLONE} --no-single-branch ${REPOSITORY_URL} ${REPOSITORY_PATH}
+    return 0
   else
-    git clone --mirror ${REPOSITORY_URL} ${REPOSITORY_PATH}
+    return 1
   fi
 }
 
-function update_mirror(){
-  local branch="$1"
+# Getting the actual commit id, in case we were passed a tag
+# or partial sha or something - it will return the sha if you pass a sha, too
+function get_revision(){
 
-  cd ${REPOSITORY_PATH}
-  # Update the origin URL if necessary.
-  git remote set-url origin ${REPOSITORY_URL}
+  local revision=""
 
-  # Note: Requires git version 1.9 or greater
-  if [ ${SHALLOW_CLONE} -gt 0 ]
-  then
-    git fetch --depth ${SHALLOW_CLONE} origin ${branch}
-  else
-    git remote update --prune
-  fi
-}
-
-function mirror(){
-  local branch="$1"
-
-  if ( is_file_exists "${REPOSITORY_PATH}/HEAD" )
+  if (is_valid_hash ${REVISION})
     then
-      display "Mirror already exists (Updating from branch: ${branch})"
-      update_mirror ${branch}
-    else
-      echo "Mirror does not exists ... cloning"
-      cleanup "${REPOSITORY_PATH}"
-      clone_repo
+    echo ${REVISION}
+    return 0
   fi
-}
+  echo ${BRANCH}
+  exit 1
+  # Ok we propably got a branch / tag name, let's find the hash
+  revision=$(git ls-remote --quiet ${REPOSITORY} ${BRANCH})
+  revision=$(echo ${revision}|cut -d " " -f 1)
 
-function clone (){
-  local branch="$1"
-
-  # Switch from mirror strategy let's clean up ?
-  if ( is_file_exists "${REPOSITORY_PATH}/HEAD" )
-  then
-    display "Cleaning up mirror repository ..."
-    cleanup "${REPOSITORY_PATH}" true
-  fi
-
-  # We are checking if repository is empty before cloning.
-  if [ "$(ls -A ${REPOSITORY_PATH})" ];
-  then
-    display "Repository already exists, updating ..."
-    sync_repo ${REVISION} ${REPOSITORY_PATH}
+  if (is_valid_hash ${revision})
+    then
+    echo ${revision}
+    return 0
   else
-    display "Repository does not exists ... cloning"
-    checkout ${REMOTE} ${branch} ${REVISION} ${REPOSITORY_PATH}
+    return 1
   fi
 }
 
 function do_release(){
-  display "Running with ${STRATEGY} strategy"
-  cleanup ${RELEASE_PATH}
+  local revision=$(get_revision)
 
-  if [ "${STRATEGY}" == "mirror" ]
-  then
-    mirror ${BRANCH}
-  elif [ "${STRATEGY}" == "clone" ]
-  then
-    clone ${BRANCH}
-  else
-    display "Unknown strategy ${STRATEGY}"
-    exit 1
-  fi
-
-  display "Preparing archive from ${BRANCH}:${REVISION}"
-  cd ${REPOSITORY_PATH} && \
-    $(git archive ${REVISION} | tar -x -f - -C ${RELEASE_PATH})
-
-  # We add a file with the hash of the current build release
-  mark ${REVISION}
+  echo ${revision}
 }
 
-setup
-do_release
+###################
+# Options         #
+###################
+
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+
+  case $key in
+    -v|--version)
+      display "Git releaser version ${VERSION}"
+      shift # past argument
+      shift # past value
+    ;;
+    -h | --help)
+      releaser_Usage
+      shift # past argument
+      shift # past value
+    ;;
+    -b | --branch)
+      if [ "${2:-}" = "" ]
+      then
+        display_error "You need to specified a branch name with --branch parameter"
+        exit 1
+      else
+        BRANCH=${2}
+      fi
+      shift # past argument
+    ;;
+    -*)
+      echo $1
+      exit 1
+    ;;
+    *) # Unknown option
+      releaser_Usage
+      exit 0
+    ;;
+  esac
+done
+
+if [ $# -lt 1 ]
+then
+  releaser_Usage
+else
+  setup
+  do_release
+fi
